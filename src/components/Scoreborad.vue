a4dbc5820e62247cd64a57c459bffa7a5cfdfc0e
George Higbie <george.higbie@Perficient.com>
2018-08-03 16:04:39 -0600

    Add scoreboard component

---
+++ src/components/Scoreborad.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="scoreboard">
+      <h1>Scoreboard</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Scoreboard',
+  data(){
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
