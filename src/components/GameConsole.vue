391ffe22acaab7ca379be0a118625fdd7311457b
George Higbie <george.higbie@Perficient.com>
2018-08-03 16:06:42 -0600

    Add gameconsole component

---
+++ src/components/GameConsole.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="game-console">
+        <h1>Game Console</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'GameConsole',
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
