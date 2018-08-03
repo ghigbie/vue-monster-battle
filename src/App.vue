efd3b38c91abd59be66bd16885b070803924be98
George Higbie <george.higbie@Perficient.com>
2018-08-03 16:11:40 -0600

    Configure root component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <Scoreboard />
+    <GameConsole />
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Scoreboard from '@/components/Scoreboard'
+import GameConsole from '@/components/GameConsole'
 
 export default {
   name: 'app',
@@ -16,13 +18,6 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
