d8d8b53d302f730f46808ef3aa189d32ce1d31e7
ghigbie <gehigbie@gmail.com>
2018-08-03 19:49:05 -0600

    Add header markup and script

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="header">
+        <h2>Header</h2>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Header',
+    props: ['title'],
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
