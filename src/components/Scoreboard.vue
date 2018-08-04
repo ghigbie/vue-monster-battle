e4f2b1e78b423dd43f3706c7770fb4d217d3e99d
ghigbie <gehigbie@gmail.com>
2018-08-03 19:34:05 -0600

    Add bootstrap file to public

--- src/components/Scoreboard.vue
+++ src/components/Scoreboard.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="scoreboard">
-      <h1>Scoreboard</h1>
       <Player :name="player1.name" :score="player1.score"/>
       <Player :name="player2.name" :score="player2.score"/>
   </div>
