4f85cc85c792cab5df2238bedda19919a492f2d2
George Higbie <george.higbie@Perficient.com>
2018-08-06 07:37:55 -0600

    Add new dataconsole component

---
+++ src/components/DataConsole.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="data-console">
+        Data Console
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'DataConsole',
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
