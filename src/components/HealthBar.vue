5649f08a57633898f66f41cdb0dd04ead2588f40
George Higbie <george.higbie@Perficient.com>
2018-08-06 08:56:07 -0600

    Add markup to healthbar

---
+++ src/components/HealthBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="health-bar">
+        HealthBar
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HealthBar',
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+
+
+<style>
+
+</style>
