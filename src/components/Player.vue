e6b71dd2df3077103a1bae9891e5613114ae3a7c
George Higbie <george.higbie@Perficient.com>
2018-08-06 09:51:16 -0600

    Remove health bar component

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,7 +1,13 @@
 <template>
-    <div class="player">
+    <div class="player center-text">
         <h1>{{ name }}</h1>
         <h2>{{ score }}</h2>
+        <div class="healthbar-outer">
+            <div class="healthbar"
+                :style="{width: score + '%'}">
+                <h3 class="inner-text">{{ score }}</h3>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -18,5 +24,22 @@ export default {
 </script>
 
 <style scoped>
-
+    .healthbar-outer{
+        background-color: rgb(72, 83, 83);
+        width: 95%;
+        margin: 1vh auto;
+        text-align: center;
+    }
+    .healthbar{
+        background-color: green;
+        margin: 0;
+        color: #fff;
+        height: 5vh;
+        width: 100%;
+        text-align: center;
+    }
+    .inner-text{
+        position: absolute;
+        left: 47.5%;
+    }
 </style>
